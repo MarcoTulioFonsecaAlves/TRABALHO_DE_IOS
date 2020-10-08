0bcb787668f60843c06d6692ffbbbe4e67f8a867
PedroNeb <11800852@aluno.cotemig.com.br>
2020-10-07 21:14:42 -0300

    Teste

---
+++ TRABALHO3D2-MARCO_KAIQUE/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  TRABALHO3D2-MARCO_KAIQUE
+//
+//  Created by COTEMIG on 07/10/20.
+//  Copyright © 2020 Cotemig. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let cameras = ["Câmera EOS Rebel SL3 com Lente EF-S 18-55mm","Câmera EOS Rebel SL3 com Lente EF-S 18-55mm","Canon EOS Rebel T7 Ef-S 18-55 F/3.5-5.6 Is II - Câmera Digital, Preto","DC101 24MP 16X Zoom Foco 1080 P HD 3.0 Polegada TFT Digital SLR Câmera com Lente Macro"]
+    
+    
+    @IBOutlet var teste: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
--- TRABALHO3D2-MARCO_KAIQUE/ViewController.swift
+++ TRABALHO3D2-MARCO_KAIQUE/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let cameras = ["Câmera EOS Rebel SL3 com Lente EF-S 18-55mm","Câmera EOS Rebel SL3 com Lente EF-S 18-55mm","Canon EOS Rebel T7 Ef-S 18-55 F/3.5-5.6 Is II - Câmera Digital, Preto","DC101 24MP 16X Zoom Foco 1080 P HD 3.0 Polegada TFT Digital SLR Câmera com Lente Macro"]
 
+    
+    @IBOutlet var Senha: UITextField!
+    @IBOutlet var Usuario: UITextField!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cameras.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId")!
+        cell.textLabel?.text = cameras[indexPath.row]
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    @IBAction func actionLogin(_ sender: Any) {
+        if Usuario.text == "x" && Senha.text == "1234" {
+            performSegue(withIdentifier: "loginSegue", sender: nil)
+        }else{
+            let alert = UIAlertController(title: "Alerta",
+                                          message: "Usurio ou Senha Invalidos",
+                                          preferredStyle: .alert)
+            
+            let acaoCancel = UIAlertAction(title: "Cancelar", style: .cancel)
+            
+            alert.addAction(acaoCancel)
+            present(alert, animated: true)
+        }
+    }
+    
 }
 
